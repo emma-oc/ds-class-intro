5a353a41efa6d95e965d765cae6e2542d66d6588
Pomelo <xingyouj@uci.edu>
2020-04-20 11:28:04 -0700

    Summary: complete hw01

--- class02/ex6.py
+++ class02/ex6.py
@@ -1,88 +1,90 @@
-'''
-Edit this file to complete Exercise 6
-'''
-
 def calculation(a, b):
-	'''
-	Write a function calculation() such that it can accept two variables 
-	and calculate the addition and subtraction of it. 
-	It must return both addition and subtraction in a single return call
+    '''
+    Write a function calculation() such that it can accept two variables 
+    and calculate the addition and subtraction of it. 
+    It must return both addition and subtraction in a single return call
 
-	Expected output:
-	res = calculation(40, 10)
-	print(res)
-	>>> (50, 30)
+    Expected output:
+    res = calculation(40, 10)
+    print(res)
+    >>> (50, 30)
 
-	Arguments:
-	a: first number 
-	b: second number
+    Arguments:
+    a: first number 
+    b: second number
 
-	Returns:
-	sum: sum of two numbers
-	diff: difference of two numbers
-	'''
+    Returns:
+    sum: sum of two numbers
+    diff: difference of two numbers
+    '''
 
-	# code up your solution here
-
+    # code up your solution here
+    return (a + b, a - b)
 
 
 def triangle_lambda():
-	'''
-	Return a lambda object that takes in a base and height of triangle
-	and computes the area.
+    '''
+    Return a lambda object that takes in a base and height of triangle
+    and computes the area.
 
-	Arguments:
-	None
+    Arguments:
+    None
 
-	Returns:
-	lambda_triangle_area: the lambda
-	'''
-
+    Returns:
+    lambda_triangle_area: the lambda
+    '''
+    return lambda base, height: (base * height) / 2
 
 
 def sort_words(hyphen_str):
-	'''
-	Write a Python program that accepts a hyphen-separated sequence of words 
-	as input, and prints the words in a hyphen-separated sequence after 
-	sorting them alphabetically.
+    '''
+    Write a Python program that accepts a hyphen-separated sequence of words 
+    as input, and prints the words in a hyphen-separated sequence after 
+    sorting them alphabetically.
 
-	Expected output:
-	sort_words('green-red-yellow-black-white')
-	>>> 'black-green-red-white-yellow'
-	
-	Arguments:
-	hyphen_str: input string separated by hyphen
+    Expected output:
+    sort_words('green-red-yellow-black-white')
+    >>> 'black-green-red-white-yellow'
 
-	Returns:
-	sorted_str: string in a hyphen-separated sequence after 
-	sorting them alphabetically
-	'''
+    Arguments:
+    hyphen_str: input string separated by hyphen
 
-	# code up your solution here
+    Returns:
+    sorted_str: string in a hyphen-separated sequence after 
+    sorting them alphabetically
+    '''
+    # code up your solution here
+    colors = hyphen_str.split('-')
+    colors.sort()
+    print('-'.join(colors))
 
 
+def perfect_number(n):
+    '''
+    Write a Python function to check whether a number is perfect or not.
 
-def perfect_number():
-	'''
-	Write a Python function to check whether a number is perfect or not.
+    A perfect number is a positive integer that is equal to the sum of 
+    its proper positive divisors. Equivalently, a perfect number is a number 
+    that is half the sum of all of its positive divisors (including itself).
 
-	A perfect number is a positive integer that is equal to the sum of 
-	its proper positive divisors. Equivalently, a perfect number is a number 
-	that is half the sum of all of its positive divisors (including itself).
+    Example: 6 is a perfect number as 1+2+3=6. Also by the second definition,
+    (1+2+3+6)/2=6. Next perfect number is 28=1+2+4+7+14. Next two perfect
+    numbers are 496 and 8128.
 
-	Example: 6 is a perfect number as 1+2+3=6. Also by the second definition,
-	(1+2+3+6)/2=6. Next perfect number is 28=1+2+4+7+14. Next two perfect
-	numbers are 496 and 8128.
+    Argument:
+    number: number to check
 
-	Argument:
-	number: number to check
+    Returns:
+    perfect: boolean, True if number is perfect
+    '''
 
-	Returns:
-	perfect: boolean, True if number is perfect
-	'''
-
-	# code up your answer here
+    # code up your answer here
+    sum = 0
+    for i in range(1, n):
+        if n % i == 0:
+            sum += i
+    return sum == n
 
 
 if __name__ == '__main__':
-	pass
+    pass
