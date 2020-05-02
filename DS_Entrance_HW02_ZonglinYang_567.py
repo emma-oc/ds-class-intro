9c5d5ec484fb9cd24adbfec105debc43df17107e
Michigan Huron <zongyang59@gmail.com>
2020-05-02 04:00:33 -0500

    Homework02-Exercise567

--- DS_Entrance_HW02_ZonglinYang_567.py
+++ DS_Entrance_HW02_ZonglinYang_567.py
@@ -223,6 +223,163 @@ def perfect_number(number):
 
 perfect_number(6)
 
+# --------------------------------------------------------------------------------------
+
+# Exercise 07
+
+# Question 01
+	'''
+	check if the input path exists, if it exists, return True and a
+	list containing the following
+		1. if this path is absolute path
+		2. if this path is a directory
+		3. if this path is a file
+	if input path doesn't exist, return False and empty list
+
+	Arguments:
+	path: input path as a string
+
+	Returns:
+	exist_flag: True if path exist, False otherwise
+	path_info_list: list containing 3 boolean varaibles for 3 checks
+		performed if path exists, return empty list if path doesn't
+		exist.
+
+	Example:
+	check_path('some/path/of/a/directory')
+	# if it exists
+	>>> True, [False, True, False]
+
+	# if doesn't exist
+	>>> False, []
+	'''
+    # code up your solution here
+    
+import os.path
+
+def check_path(path):
+    if os.path.exists(path):
+        if os.path.isabs(path):
+            p_absolute = True
+        else:
+            p_absolute = False
+        
+        if os.path.isdir(path):
+            p_directory = True
+        else:
+            p_directory = False
+        
+        if os.path.isfile(path):
+            p_file = True
+        else:
+            p_file = False
+        
+        return True, [p_absolute, p_directory, p_file]
+    
+    else:
+        return False, []    
+    
+check_path('C:\\Users\\13125\\Desktop\\2020Spring\\CSC555-DataMining')
+check_path('D:\\Users\\13125\\Desktop')
+
+
+# Question 02
+	'''
+	reads in a csv file then return the total number of lines in it
+
+	Arguments:
+	file: path to the file to read
+
+	Returns:
+	Number of rows in the input file
+
+	Example:
+	read_csv('AMZN.csv')
+	>>> 14
+	'''
+	# code up your solution here
+
+import os
+os.getcwd()
+os.chdir('C:\\Users\\13125\\Desktop')
+
+def read_csv(file):
+    with open(file) as f:
+        counter = 0
+        for line in f:
+            counter += 1
+    return counter
+    
+read_csv('AMZN.csv')
+
+
+# Question 03
+	'''
+	write out a csv file for the data list (structed as list of list), 
+	where each item in the data_list is a row in output file, and 
+	every element in the sublist is separated by comma
+
+	Arguments:
+	data_list: input data list, each elemet is a list representing 
+		a row with values for each column
+	file: path to save the csv file 
+
+	Returns:
+	None
+
+	Example:
+	data_list = [(1,2,3,4), (5,6,7,8), (9,10,11,12)]
+	write_csv(data_list, 'example.csv')
+	
+	'example.csv' looks like below:
+	1,2,3,4
+	5,6,7,8
+	9,10,11,12
+	'''
+	# code up your solution here
+import csv
+
+def write_csv(data_list, output_file):
+    fd = open(output_file, 'w', newline='')
+    with fd:
+        csv.writer(fd).writerows(data_list)
+        
+data_list = [(1,2,3,4), (5,6,7,8), (9,10,11,12)]
+write_csv(data_list, 'example.csv')
+
+
+# Question 04
+	'''
+	reads a JSON file and return its contents as a dictionary
+
+	Arguments:
+	file: path to the file to read
+
+	Returns:
+	A dictionary containing JSON contents
+
+	Example:
+	read_json('some.json')
+	>>> [{'name': 'emma', 'skill': {'coding1': 'python', 'coding2': 'r'}, 'role': 0}]
+	'''
+	# code up you solution here
+import json
+
+def read_json(file):
+    fd = open(file)
+    content = json.load(fd)
+    
+    print(content)
+    
+read_json('some.json')
+
+
+
+
+
+
+
+
 
 
 
