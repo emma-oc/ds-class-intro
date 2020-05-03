1be4e5a7a873e9b1c21c2c8e2619afb3506aa88f
Bin Sun <bs675@mst.edu>
2020-05-03 00:31:38 -0500

    upload homework02

--- python_basics/class02/exercise_5.py
+++ python_basics/class02/exercise_5.py
@@ -1,14 +1,19 @@
-'''
-Edit this file to complete Exercise 5
-'''
+#!/usr/bin/env python
+# coding: utf-8
 
-# 1. rite a Python program to find those numbers which are divisible by 7 and multiple of 5, between 1500 and 2700 (both included).
+# In[10]:
+
+
+# 1.  Write a Python program to find those numbers which are divisible by 7 and multiple of 5, between 1500 and 2700 (both included).
 
 # code up your solution here
 
-
-
-
+list1 = []
+for i in range(1500, 2701):
+    if i % 7 == 0 and i %5 ==0:
+        list1.append(i)
+print(len(list1))
+list1[:5]
 
 # 2. Write a Python program to count the number of even and odd numbers from a series of numbers.
 
@@ -19,10 +24,19 @@ Edit this file to complete Exercise 5
 
 # code up your solution here
 
+counter_even=0
+counter_odd=0
+list2=[1, 2, 3, 4, 5, 6, 7, 8, 9]
+for i in list2:
+    if i%2==0:
+        counter_even+=1
+    else:
+        counter_odd+=1
+print(counter_even)
+print(counter_odd)
 
-
-
-# 3. Write a Python program which iterates the integers from 0 to 50. For multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz". For numbers which are multiples of both three and five print "FizzBuzz".
+# 3. Write a Python program which iterates the integers from 0 to 50. 
+# For multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz". For numbers which are multiples of both three and five print "FizzBuzz".
 
 # Expected Output :
 # >>> fizzbuzz
@@ -34,40 +48,69 @@ Edit this file to complete Exercise 5
 # >>> ...
 
 # code up your solution here
-
-
-
-
+for i in range(0, 51):
+    if i % 15==0:
+        print('FizzBuzz')
+    elif i % 3==0:
+        print('Fizz')
+    elif i % 5==0:
+        print('Buzz')
+    else:
+        print(i)
 
 # 4. Given a list iterate it and display numbers which are divisible by 5 and if you find number greater than 150 stop the loop iteration
 
 # examples: list1 = [12, 15, 32, 42, 55, 75, 122, 132, 150, 180, 200]
-# output:
-# >>> 15
-# >>> 55
-# >>> 75
-# >>> 150
 
-# code up your solution here
+list3 = [12, 15, 32, 42, 55, 75, 122, 132, 150, 180, 200]
 
 
-
+for i in list3:
+    if i%5==0:
+        if i <= 150:
+            print(i)
+        else:
+            break
 
 # 5. Pick one of the questions above and use range() for a different solution
 
 # code up your solution here
 
 
+list4 = [12, 15, 32, 42, 55, 75, 122, 132, 150, 180, 200]
 
 
+for i in range(len(list3)):
+    if list3[i]%5==0:
+        if list3[i] <= 150:
+            print(list3[i])
+        else:
+            break
+            
 # 6. Pick one of the question above and use comprehension for a different solution
 
 # code up your solution here
 
+# 1:
+list5 = [i for i in range(1500, 2701) if i % 7 == 0 and i %5 ==0]
 
+list5[:5]
 
 
 # 7. Pcik one of the questions above and use while loop for a different solution
 
 # code up your solution here
+
+#3:
+i=0
+while i <=50:
+    if i % 15==0:
+        print('FizzBuzz')
+    elif i % 3==0:
+        print('Fizz')
+    elif i % 5==0:
+        print('Buzz')
+    else:
+        print(i)
+    i+=1
 
--- python_basics/class03/exercise_7.py
+++ python_basics/class03/exercise_7.py
@@ -1,100 +1,122 @@
-'''
-Edit this file to complete Exercise 7
-'''
+#!/usr/bin/env python
+# coding: utf-8
 
-# import the modules you need here
+# In[52]:
 
 
+import os
+import csv
+import json
 def check_path(path):
-	'''
-	check if the input path exists, if it exists, return True and a
-	list containing the following
-		1. if this path is absolute path
-		2. if this path is a directory
-		3. if this path is a file
-	if input path doesn't exist, return False and empty list
+    '''
+    check if the input path exists, if it exists, return True and a
+    list containing the following
+        1. if this path is absolute path
+        2. if this path is a directory
+        3. if this path is a file
+    if input path doesn't exist, return False and empty list
 
-	Arguments:
-	path: input path as a string
+    Arguments:
+    path: input path as a string
 
-	Returns:
-	exist_flag: True if path exist, False otherwise
-	path_info_list: list containing 3 boolean varaibles for 3 checks
-		performed if path exists, return empty list if path doesn't
-		exist.
+    Returns:
+    exist_flag: True if path exist, False otherwise
+    path_info_list: list containing 3 boolean varaibles for 3 checks
+        performed if path exists, return empty list if path doesn't
+        exist.
 
-	Example:
-	check_path('some/path/of/a/directory')
-	# if it exists
-	>>> True, [False, True, False]
+    Example:
+    check_path('some/path/of/a/directory')
+    # if it exists
+    >>> True, [False, True, False]
 
-	# if doesn't exist
-	>>> False, []
-	'''
-
+    # if doesn't exist
+    >>> False, []
+    
+    '''
+    if os.path.exists(a):
+        isabs = os.path.isabs(a)
+        isdir = os.path.isdir(a)
+        isfile = os.path.isfile(a)
+    
+        return True, [isabs, isdir, isfile]
+    
+    else:
+        return False, []
 
 def read_csv(file):
-	'''
-	reads in a csv file then return the total number of lines in it
+    '''
+    reads in a csv file then return the total number of lines in it
 
-	Arguments:
-	file: path to the file to read
+    Arguments:
+    file: path to the file to read
 
-	Returns:
-	Number of rows in the input file
+    Returns:
+    Number of rows in the input file
 
-	Example:
-	read_csv('AMZN.csv')
-	>>> 14
-	'''
+    Example:
+    read_csv('AMZN.csv')
+    >>> 14
+    '''
 
-	# code up your solution here
+    # code up your solution here
+    counts=0
+    with open(file, 'r') as file:
+        for line in file:
+            counts+=1
+    return counts
 
-
+    
 def write_csv(data_list, output_file):
-	'''
-	write out a csv file for the data list (structed as list of list), 
-	where each item in the data_list is a row in output file, and 
-	every element in the sublist is separated by comma
+    '''
+    write out a csv file for the data list (structed as list of list), 
+    where each item in the data_list is a row in output file, and 
+    every element in the sublist is separated by comma
 
-	Arguments:
-	data_list: input data list, each elemet is a list representing 
-		a row with values for each column
-	file: path to save the csv file 
+    Arguments:
+    data_list: input data list, each elemet is a list representing 
+        a row with values for each column
+    file: path to save the csv file 
 
-	Returns:
-	None
+    Returns:
+    None
 
-	Example:
-	data_list = [(1,2,3,4), (5,6,7,8), (9,10,11,12)]
-	write_csv(data_list, 'example.csv')
-	
-	'example.csv' looks like below:
-	1,2,3,4
-	5,6,7,8
-	9,10,11,12
-	'''
+    Example:
+    data_list = [(1,2,3,4), (5,6,7,8), (9,10,11,12)]
+    write_csv(data_list, 'example.csv')
 
-	# code up your solution here
-
+    'example.csv' looks like below:
+    1,2,3,4
+    5,6,7,8
+    9,10,11,12
+    '''
+    
+    with open(output_file, 'w', newline = '') as myfile:
+        wr = csv.writer(myfile)
+        for i in data_list:
+            wr.writerow(i)
+            
 
 def read_json(file):
-	'''
-	reads a JSON file and return its contents as a dictionary
+    '''
+    reads a JSON file and return its contents as a dictionary
 
-	Arguments:
-	file: path to the file to read
+    Arguments:
+    file: path to the file to read
 
-	Returns:
-	A dictionary containing JSON contents
+    Returns:
+    A dictionary containing JSON contents
 
-	Example:
-	read_json('some.json')
-	>>> [{'name': 'emma', 'skill': {'coding1': 'python', 'coding2': 'r'}, 'role': 0}]
-	'''
+    Example:
+    read_json('some.json')
+    >>> [{'name': 'emma', 'skill': {'coding1': 'python', 'coding2': 'r'}, 'role': 0}]
+    '''
 
-	# code up you solution here
-
+    # code up you solution here
+    with open(file) as f:
+        js = json.load(f)
+    return js
 
 if __name__=="__main__":
-	pass
+    pass
+
