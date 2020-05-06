5a767fe4fa7fa9fe3c87bcf6c10b5b74dd07dc5b
Laura Wang <Laura@lauras-macbook.home>
2020-05-06 18:57:31 -0400

    here is exercise 7

---
+++ python_basics/class03/Ex7.py
@@ -0,0 +1,116 @@
+
+'''
+Edit this file to complete Exercise 7
+'''
+
+# import the modules you need here
+
+
+def check_path(path):
+	'''
+	check if the input path exists, if it exists, return True and a
+	list containing the following
+		1. if this path is absolute path
+		2. if this path is a directory
+		3. if this path is a file
+	if input path doesn't exist, return False and empty list
+	Arguments:
+	path: input path as a string
+	Returns:
+	exist_flag: True if path exist, False otherwise
+	path_info_list: list containing 3 boolean varaibles for 3 checks
+		performed if path exists, return empty list if path doesn't
+		exist.
+	Example:
+	check_path('some/path/of/a/directory')
+	# if it exists
+	>>> True, [False, True, False]
+	# if doesn't exist
+	>>> False, []
+	'''
+	import os
+	import os.path
+	from os import path
+	if os.path.exists(path) == True:
+		list1 = []
+		ab = os.path.isabs(path)
+		list1.append(ab)
+		di = os.path.isdir(path)
+		list1.append(di)
+		fi = os.path.isfile(path)
+		list1.append(fi)
+		print(True, list1)
+	else:
+		print(False, [])
+
+
+def read_csv(file):
+	'''
+	reads in a csv file then return the total number of lines in it
+	Arguments:
+	file: path to the file to read
+	Returns:
+	Number of rows in the input file
+	Example:
+	read_csv('AMZN.csv')
+	>>> 14
+	'''
+
+	# code up your solution here
+	import csv
+	data = []
+	with open(file, 'r') as f:
+    	reader = csv.reader(f, delimiter=',')  # change delimiter
+    	for row in reader:
+       		data.append(row)
+    print(len(data))
+
+def write_csv(data_list, output_file):
+	'''
+	write out a csv file for the data list (structed as list of list), 
+	where each item in the data_list is a row in output file, and 
+	every element in the sublist is separated by comma
+	Arguments:
+	data_list: input data list, each elemet is a list representing 
+		a row with values for each column
+	file: path to save the csv file 
+	Returns:
+	None
+	Example:
+	data_list = [(1,2,3,4), (5,6,7,8), (9,10,11,12)]
+	write_csv(data_list, 'example.csv')
+	
+	'example.csv' looks like below:
+	1,2,3,4
+	5,6,7,8
+	9,10,11,12
+	'''
+
+	# code up your solution here
+	import csv
+	with open(output, 'w', newline='') as f:
+    writer = csv.writer(f)
+    for i in range(len(data)):
+        row = data[i]
+        writer.writerow(row)
+
+def read_json(file):
+	'''
+	reads a JSON file and return its contents as a dictionary
+	Arguments:
+	file: path to the file to read
+	Returns:
+	A dictionary containing JSON contents
+	Example:
+	read_json('some.json')
+	>>> [{'name': 'emma', 'skill': {'coding1': 'python', 'coding2': 'r'}, 'role': 0}]
+	'''
+
+	# code up you solution here
+	import json
+	with open(file) as f:
+  	  js = json.load(f)
+	print(js)
+
+if __name__=="__main__":
+	pass
